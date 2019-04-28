f7223d9ebbfad41d84cb4990503c5993be25e032
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-04-28 12:25:29 -0300

    Getting summary payment

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -6,9 +6,35 @@ BillingCycle.updateOptions({ new: true, runValidators: true });
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
         if (error) {
-            res.status(500).json({errors: [error]});
+            res.status(500).json({ errors: [error] });
         } else {
-            res.json({value});
+            res.json({ value });
+        }
+    });
+});
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([
+        {
+            $project: {
+                credit: { $sum: '$credits.value' }, debt: { $sum: '$debits.value' }
+            }
+        },
+        {
+            $group: {
+                _id: 0, credit: { $sum: '$credit' }, debt: { $sum: '$debt' }
+            }
+        },
+        {
+            $project: {
+                _id: 0, credit: 1, debt: 1
+            }
+        }
+    ]).exec((error, result) => {
+        if (error) {
+            res.status(500).json({ errors: [error] });
+        } else {
+            res.json(result[0] || { credit: 0, debt: 0 });
         }
     });
 });
